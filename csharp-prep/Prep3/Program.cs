9896e97a2175aab02a05e6b8cb31b9df35835774
EthanBrayZA <bra23004@byui.edu>
2023-06-10 18:56:30 +0200

    Compleated Prep Lessons

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int MagicNumber = randomGenerator.Next(1, 101);
+
+        int new_guess = -1;
+
+        while (new_guess != MagicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            new_guess = int.Parse(Console.ReadLine());
+
+            if (new_guess > MagicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (new_guess < MagicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } 
+        
     }
 }
